9f4dad8648863d8e89398ee095e09e6fda997f1f
gustavocadev <gustavocadev@gmail.com>
2024-03-22 17:57:24 -0500

    Add seed

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,70 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { Item } from '../items/entities/item.entity';
+import { ConfigService } from '@nestjs/config';
+import { SEED_ITEMS, SEED_USERS } from './data/seed-data';
+import { UsersService } from '../users/users.service';
+import { ItemsService } from '../items/items.service';
+
+@Injectable()
+export class SeedService {
+  private isProd: boolean;
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    @InjectRepository(Item)
+    private readonly itemRepository: Repository<Item>,
+
+    private readonly configService: ConfigService,
+
+    private readonly userService: UsersService,
+    private readonly itemService: ItemsService,
+  ) {
+    this.isProd = this.configService.get('STATE') === 'prod';
+  }
+  async executeSeed(): Promise<boolean> {
+    if (this.isProd)
+      throw new UnauthorizedException('Not allowed in production');
+
+    // 1. Clean all data
+    await this.deleteDatabase();
+
+    // 2. Add data
+    const users = await this.loadUsers();
+    await this.loadItems(users);
+
+    return true;
+  }
+
+  async deleteDatabase(): Promise<void> {
+    await this.itemRepository.delete({});
+    await this.userRepository.delete({});
+  }
+
+  async loadUsers(): Promise<User[]> {
+    const users = [];
+    for (const user of SEED_USERS) {
+      const newUser = await this.userService.create(user);
+      users.push(newUser);
+    }
+    return users;
+  }
+
+  async loadItems(users: User[]): Promise<void> {
+    const items = [];
+    for (const item of SEED_ITEMS) {
+      const randomIdx = Math.round(Math.random() * users.length);
+      const user = users.at(randomIdx);
+
+      const newItem = this.itemService.create(item, user);
+
+      items.push(newItem);
+    }
+
+    Promise.all(items);
+  }
+}
