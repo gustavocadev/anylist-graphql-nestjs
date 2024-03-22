4ad16d3c9110a4ac206c8e9c2981d6f8f5816de2
gustavocadev <gustavocadev@gmail.com>
2024-03-22 17:58:39 -0500

    Add user entity and update item service methods

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -4,6 +4,7 @@ import { UpdateItemInput } from './dto/inputs/update-item.input';
 import { Repository } from 'typeorm';
 import { Item } from './entities/item.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class ItemsService {
@@ -12,38 +13,62 @@ export class ItemsService {
     private readonly itemsRespository: Repository<Item>,
   ) {}
 
-  async create(createItemInput: CreateItemInput): Promise<Item> {
+  async create(createItemInput: CreateItemInput, user: User): Promise<Item> {
     const item = this.itemsRespository.create(createItemInput);
+
+    item.userId = user;
+
     return this.itemsRespository.save(item);
   }
 
-  findAll() {
-    return this.itemsRespository.find();
+  findAll(user: User) {
+    return this.itemsRespository.find({
+      where: {
+        userId: user,
+      },
+    });
   }
 
-  findOne(id: string): Promise<Item> {
-    const product = this.itemsRespository.findOneBy({ id });
-
-    if (!product) throw new NotFoundException(`Product #${id} not found`);
-
-    return product;
-  }
-
-  async update(id: string, updateItemInput: UpdateItemInput): Promise<Item> {
-    await this.itemsRespository.update(id, updateItemInput);
-    const product = await this.findOne(id);
-
-    if (!product) throw new NotFoundException(`Product #${id} not found`);
-
-    return product;
-  }
-
-  async remove(id: string) {
-    const item = await this.itemsRespository.findOneBy({ id });
-    await this.itemsRespository.delete({
-      id,
+  async findOne(id: string, user: User): Promise<Item> {
+    // load relation as well
+    const product = await this.itemsRespository.findOne({
+      relations: {
+        userId: true,
+      },
+      where: {
+        id,
+        userId: user,
+      },
     });
 
+    if (!product) throw new NotFoundException(`Product #${id} not found`);
+
+    return product;
+  }
+
+  async update(
+    id: string,
+    updateItemInput: UpdateItemInput,
+    user: User,
+  ): Promise<Item> {
+    const product = await this.findOne(id, user);
+    await this.itemsRespository.update(id, updateItemInput);
+
+    return product;
+  }
+
+  async remove(id: string, user: User) {
+    const item = await this.findOne(id, user);
+    await this.itemsRespository.remove(item);
+
     return item;
+  }
+
+  async itemCountByUser(user: User): Promise<number> {
+    return this.itemsRespository.count({
+      where: {
+        userId: user,
+      },
+    });
   }
 }
