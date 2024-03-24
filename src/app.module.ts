111c52e3cfaffa29f1ae0a5e36ab4c0b6465d19b
gustavocadev <gustavocadev@gmail.com>
2024-03-23 19:13:26 -0500

    Add ListItemModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { JwtService } from '@nestjs/jwt';
 import { SeedModule } from './seed/seed.module';
 import { CommonModule } from './common/common.module';
 import { ListsModule } from './lists/lists.module';
+import { ListItemModule } from './list-item/list-item.module';
 
 @Module({
   imports: [
@@ -62,6 +63,8 @@ import { ListsModule } from './lists/lists.module';
     CommonModule,
 
     ListsModule,
+
+    ListItemModule,
   ],
   controllers: [],
   providers: [],
